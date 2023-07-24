f4fb1c8e39e232629160d18db6f071b230b91f00
alivejuicy <kkddhh1998@gmail.com>
2023-07-24 13:06:49 +0900

    Fix: 로그인 시 reqeustLogin dto에 email도 포함해서 요청하기

--- src/main/java/com/example/demo/src/auth/dto/RequestLogin.java
+++ src/main/java/com/example/demo/src/auth/dto/RequestLogin.java
@@ -11,7 +11,8 @@ public record RequestLogin(
         String username,
         @NotNull
         @Size(min = 5, max = 100)
-        String password
+        String password,
+        String email
 ) {
 
 
