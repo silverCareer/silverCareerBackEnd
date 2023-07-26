943f4f157ddf6be0b3a825b8d9ff73e998c880a4
kylesung0520 <kylesung0520@gmail.com>
2023-07-26 18:53:27 +0900

    ë배포용 인스턴스 내 Front, Back 프ë¡각ê각의 이미지 Docker 생성을 위한 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,14 @@
 # 가동중인 silvercareer 도커 중단 및 삭제
-sudo docker ps -a -q --filter "name=silvercareer" | grep -q . && docker stop silvercareer && docker rm silvercareer | true
+sudo docker ps -a -q --filter "name=silvercareerBackend" | grep -q . && docker stop silvercareerBackend && docker rm silvercareerBackend | true
 
 # 기존 이미지 삭제
-sudo docker rmi kylesung0520/silvercareer:1.0
+sudo docker rmi kylesung0520/silvercareer:backend
 
 # 도커허브 이미지 pull
-sudo docker pull kylesung0520/sivlercareer:1.0
+sudo docker pull kylesung0520/silvercareer:backend
 
 # 도커 run
-docker run -d -p 8080:8080 --name silvercareer kylesung0520/silvercareer:1.0
+docker run -d -p 8080:8080 --name silvercareerBackend kylesung0520/silvercareer:backend
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 docker rmi -f &(docker images -f "dangling=true" -q) || true
