74fa7711cee07d8be084f6fe527555e8a36c1eaf
YEONJEONG BAEK <84756243+baek0402@users.noreply.github.com>
2023-07-31 11:22:06 +0900

    Feature:카카오 로그인 api, 파일구조

--- src/main/java/com/example/demo/src/kakao/ResponseLoginRegister.java
+++ src/main/java/com/example/demo/src/kakao/ResponseLoginRegister.java
@@ -1,29 +1,25 @@
 package com.example.demo.src.kakao;
 
 import com.example.demo.src.auth.dto.ResponseToken;
-import com.example.demo.src.kakao.dto.ResponseUser;
+import com.example.demo.src.auth.dto.ResponseUser;
+import com.example.demo.src.kakao.dto.KakaoUser;
 import lombok.Builder;
-
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Builder
 public record ResponseLoginRegister(
         Long userId,
         String userEmail,
         String userImage,
-        Long age,
         String provider,
         String jwtAccessToken,
         String jwtRefreshToken
 ) {
-    public static ResponseLoginRegister of(ResponseUser user, ResponseToken token) {
+    public static ResponseLoginRegister of(KakaoUser user, ResponseToken token) {
         if (user == null) return null;
         return ResponseLoginRegister.builder()
                 .userId(user.userIdx())
                 .userEmail(user.userEmail())
                 .userImage(user.userImage())
-                .age(user.age())
                 .provider(user.provider())
                 .jwtAccessToken(token.accessToken())
                 .jwtRefreshToken(token.refreshToken())
--- src/main/java/com/example/demo/src/kakao/dto/RequestUser.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.src.kakao.dto;
-
-import lombok.Builder;
-
-@Builder
-public record RequestUser (
-
-){
-}
