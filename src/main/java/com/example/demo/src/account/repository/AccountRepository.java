25c49e8ddc9456b89f4e5acf51eb74015d3cdf8e
JaeHeon Jeong <drdd1120@naver.com>
2023-08-04 13:03:39 +0900

    feature: 계좌 잔액 충전

---
+++ src/main/java/com/example/demo/src/account/repository/AccountRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.src.account.repository;
+
+import com.example.demo.src.account.domain.Account;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByMember_MemberIdx(Long authIdx);
+}
