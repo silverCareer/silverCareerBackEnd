807c533f099eee958259c46c3e518842d20a78c3
sss_hun <88662101+seonghunhan@users.noreply.github.com>
2023-08-09 16:16:17 +0900

    Feat : 회원가입시 인증번호 로직 추가 및 관련 시크릿키 설정 (#37)
    Co-authored-by: Kwak donghyun <80264449+alivejuicy@users.noreply.github.com>

--- src/main/java/com/example/demo/src/member/controller/MemberController.java
+++ src/main/java/com/example/demo/src/member/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.example.demo.src.member.controller;
 
+import com.example.demo.global.exception.BaseException;
 import com.example.demo.global.exception.BaseResponse;
+import com.example.demo.global.exception.BaseResponseStatus;
 import com.example.demo.global.exception.dto.CommonResponse;
 import com.example.demo.global.security.CustomJwtFilter;
 import com.example.demo.src.member.dto.RequestLogin;
@@ -23,6 +25,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+
 import java.io.IOException;
 
 @RestController
@@ -33,6 +36,8 @@ public class MemberController {
     private final MemberAuthService memberAuthService;
     private final MemberProvider memberProvider;
     private final SecurityUtil securityUtil;
+
+
 
     @PostMapping("/members")
     public ResponseEntity<CommonResponse> signUp(@Valid @RequestBody RequestSingUp registerDto) throws IllegalAccessException {
@@ -46,6 +51,22 @@ public class MemberController {
                 .success(true)
                 .response("회원가입 성공")
                 .build());
+    }
+
+    @GetMapping("/sendSMS/{phone}")
+    public BaseResponse<PostAuthCodeRes> sendSMS(@PathVariable String phone) {
+        try {
+            if (phone.length() != 11) {
+                return new BaseResponse<>(BaseResponseStatus.POST_USERS_UNREGEX_PHONE);
+            }
+
+            System.out.println("수신자 번호 : " + phone);
+            PostAuthCodeRes postAuthCodeRes = memberAuthService.certifiedPhoneNumber(new PostAuthCodeReq(phone));
+
+            return new BaseResponse<>(postAuthCodeRes);
+        } catch (BaseException exception) {
+            return new BaseResponse<>((exception.getStatus()));
+        }
     }
 
     @PostMapping("/login")
@@ -103,6 +124,7 @@ public class MemberController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+
 //    @PatchMapping("/updateProfileImg")
 //    public ResponseEntity updateProfileImg(@RequestParam(value="img")MultipartFile img) throws IOException{
 //        return securityUtil.getCurrentUsername()
@@ -116,4 +138,5 @@ public class MemberController {
 //                })
 //                .orElse(new ResponseEntity<>(HttpStatus.UNAUTHORIZED));
 //    }
+
 }
