81b5957d510a6cb8eca2e279f4354890abda07f3
kylesung0520 <kylesung0520@gmail.com>
2023-08-09 15:31:49 +0900

    Fix: debug error

--- src/main/java/com/example/demo/src/account/service/AccountService.java
+++ src/main/java/com/example/demo/src/account/service/AccountService.java
@@ -22,26 +22,26 @@ public class AccountService {
 
     @Transactional
     public void charge(RequestAccountCharge chargeDto) throws IllegalAccessException {
-        Member member = memberRepository.findById(chargeDto.memberIdx()).orElseThrow(()
-                -> new IllegalAccessException("해당 유저가 없습니다."));
-        Optional<Account> optionalAccount = accountRepository.findByMember_MemberIdx(chargeDto.memberIdx());
-
-        Account account = new Account();
-        long balance = chargeDto.balance();
-        if(accountProvider.validateBalance(balance)){
-            if(optionalAccount.isEmpty()){ // 아직 캐시가 없으면 최초 등록
-                account = Account.builder()
-                        .bankName(chargeDto.bankName())
-                        .accountNum(chargeDto.accountNum())
-                        .balance(balance)
-                        .member(member) // authIdx 값 설정
-                        .build();
-            } else { // 아닐 경우 누적해서
-                account = optionalAccount.get();
-                account.addBalance(balance);
-            }
-        }
-
-        accountRepository.save(account);
+//        Member member = memberRepository.findById(chargeDto.memberIdx()).orElseThrow(()
+//                -> new IllegalAccessException("해당 유저가 없습니다."));
+//        Optional<Account> optionalAccount = accountRepository.findByMember_MemberIdx(chargeDto.memberIdx());
+//
+//        Account account = new Account();
+//        long balance = chargeDto.balance();
+//        if(accountProvider.validateBalance(balance)){
+//            if(optionalAccount.isEmpty()){ // 아직 캐시가 없으면 최초 등록
+//                account = Account.builder()
+//                        .bankName(chargeDto.bankName())
+//                        .accountNum(chargeDto.accountNum())
+//                        .balance(balance)
+//                        .member(member) // authIdx 값 설정
+//                        .build();
+//            } else { // 아닐 경우 누적해서
+//                account = optionalAccount.get();
+//                account.addBalance(balance);
+//            }
+//        }
+//
+//        accountRepository.save(account);
     }
 }
