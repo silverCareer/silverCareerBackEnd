8eb311205afff562b59c5148790ddfc47859c2bb
kylesung0520 <kylesung0520@gmail.com>
2023-08-11 01:05:10 +0900

    Feature: 상품 등록 시 이미지 첨부 기능 구현, BaseResponse 사용 및 record 사용을 지양하기 위한 코드 리팩토링

--- src/main/java/com/example/demo/src/product/dto/ProductDetailReq.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.src.product.dto;
-
-import lombok.Builder;
-
-@Builder
-public record ProductDetailReq (
-        Long productIdx
-){ }
--- src/main/java/com/example/demo/src/product/service/ProductServiceImpl.java
+++ src/main/java/com/example/demo/src/product/service/ProductServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.demo.src.product.service;
 
+import com.example.demo.global.exception.ErrorCode;
+import com.example.demo.src.S3Service;
 import com.example.demo.src.member.domain.Member;
 import com.example.demo.src.member.repository.MemberRepository;
 import com.example.demo.src.product.domain.Product;
@@ -8,31 +10,39 @@ import com.example.demo.src.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class ProductServiceImpl implements ProductService{
+public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
     private final MemberRepository memberRepository;
-    @Transactional
-    public void createProduct(String username, CreateProduct createProduct) {
-        String productName = createProduct.productName();
-        String productDescription = createProduct.productDescription();
-        String category = createProduct.category();
-        Long price = createProduct.price();
-        String productImages = createProduct.productImages();
+    private final S3Service s3Service;
 
-        Member member = memberRepository.findMemberByUsername(username);
+    @Override
+    @Transactional
+    public void createProduct(String username, CreateProductReq createProductReq) throws IOException {
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ELEMENT));
+
+        MultipartFile image = createProductReq.getProductImage();
+        String imageUrl = "";
+
+        if (image != null && !image.isEmpty()) {
+            imageUrl = s3Service.upload(image, "product",
+                    username + "_" + createProductReq.getProductName());
+        }
 
         Product product = Product.builder()
-                .productName(productName)
-                .description(productDescription)
-                .category(category)
-                .price(price)
-                .image(productImages)
+                .productName(createProductReq.getProductName())
+                .description(createProductReq.getProductDescription())
+                .category(createProductReq.getCategory())
+                .price(createProductReq.getPrice())
+                .image(imageUrl)
                 .likes(0L)
                 .saleCount(0L)
                 .member(member)
@@ -41,40 +51,36 @@ public class ProductServiceImpl implements ProductService{
         productRepository.save(product);
     }
 
-    @Transactional
-    public DisplayProductRes displayProductByCategory(DisplayProductReq displayProductReq) {
-        String category = displayProductReq.category();
-        List<Product> products = category.equals("all")
+    @Override
+    @Transactional(readOnly = true)
+    public List<DisplayProductRes> displayProductByCategory(String category) {
+        List<Product> productList = category.equals("all")
                 ? productRepository.findAll()
                 : productRepository.findByCategory(category);
-        List<DisplayProductRes.ProductDto> productDtoList = products.stream().map(this::mapToProductDto).collect(Collectors.toList());
-        return DisplayProductRes.builder().products(productDtoList).build();
+
+        if (productList.isEmpty()) {
+            throw new CustomException(ErrorCode.NOT_FOUND_ELEMENT);
+        }
+
+        return productList.stream()
+                .map(DisplayProductRes::of) // 람다 표현식 사용
+                .collect(Collectors.toList());
     }
 
-    @Transactional
-    public ProductDetailRes getProductDetail(ProductDetailReq productDetailReq) {
-        Long productId = productDetailReq.productIdx();
-        Product product = productRepository.findProductByProductIdx(productId);
-        ProductDetailRes productDetailRes = ProductDetailRes.builder()
+    @Override
+    @Transactional(readOnly = true)
+    public ProductDetailRes getProductDetail(Long productId) {
+        Product product = productRepository.findProductByProductIdx(productId)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ELEMENT));
+
+        return ProductDetailRes.builder()
                 .productIdx(product.getProductIdx())
                 .productName(product.getProductName())
                 .category(product.getCategory())
                 .description(product.getDescription())
                 .price(product.getPrice())
                 .image(product.getImage())
-                .likes(product.getLikes()).build();
-
-        return productDetailRes;
-    }
-
-    private DisplayProductRes.ProductDto mapToProductDto(Product product) {
-        return DisplayProductRes.ProductDto.builder()
-                .productIdx(product.getProductIdx())
-                .productName(product.getProductName())
-                .productDescription(product.getDescription())
-                .productPrice(product.getPrice())
-                .productImage(product.getImage())
-                .productLikes(product.getLikes())
+                .likes(product.getLikes())
                 .build();
     }
 }
