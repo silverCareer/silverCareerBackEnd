e4f6edf4124baa774c9d823bf791a21d667a3f60
JaeHeon Jeong <drdd1120@naver.com>
2023-08-10 13:20:03 +0900

    Feature: 비밀번호, 전화번호 변경 구현, 이메일, 비밀번호 정규식 적용, 비밀번호 변경 시 과거 비밀번호 체크

--- src/main/java/com/example/demo/src/member/service/MemberAuthService.java
+++ src/main/java/com/example/demo/src/member/service/MemberAuthService.java
@@ -55,6 +55,12 @@ public class MemberAuthService {
         if (memberRepository.findOneWithAuthorityByUsername(registerDto.getEmail()).orElseGet(() -> null) != null) {
             throw new IllegalAccessException("이미 가입된 사용자입니다!");
         }
+        if(!isRegexEmail(registerDto.getEmail())){
+            throw new IllegalAccessException("옳바르지 않은 이메일 형식입니다.");
+        }
+        if(!isRegexPassword(registerDto.getPassword())){ // 비밀번호 정규식
+            throw new IllegalAccessException("옳바르지 않은 비밀번호 형식입니다.");
+        }
         Authority authority = Authority.builder()
                 .authorityName("ROLE_MENTOR")
                 .build();
@@ -75,10 +81,17 @@ public class MemberAuthService {
                 .bankName(registerDto.getBankName())
                 .build();
     }
+
     @Transactional
     public void menteeSignUp(final RequestSingUp registerDto) throws IllegalAccessException {
         if (memberRepository.findOneWithAuthorityByUsername(registerDto.getEmail()).orElseGet(() -> null) != null) {
             throw new IllegalAccessException("이미 가입된 사용자입니다!");
+        }
+        if(!isRegexEmail(registerDto.getEmail())){
+            throw new IllegalAccessException("옳바르지 않은 이메일 형식입니다.");
+        }
+        if(!isRegexPassword(registerDto.getPassword())){ // 비밀번호 정규식
+            throw new IllegalAccessException("옳바르지 않은 비밀번호 형식입니다.");
         }
         Authority authority = Authority.builder()
                 .authorityName("ROLE_MENTEE")
@@ -109,11 +122,11 @@ public class MemberAuthService {
 
     // 캐시 충전
     @Transactional
-    public void cashCharge(RequestCashCharge requestCashCharge, String memberEmail) throws IllegalAccessException{
+    public void cashCharge(RequestCashCharge requestCashCharge, String memberEmail) throws IllegalAccessException {
         Member member = memberRepository.findByUsername(memberEmail).orElseThrow(()
                 -> new IllegalAccessException("해당 유저가 없습니다."));
         long amount = requestCashCharge.getBalance();
-        if(validateCash(amount)){
+        if (validateCash(amount)) {
             memberAccountDeductEvent(memberEmail, amount);
         } else {
             throw new IllegalAccessException("양수만 입력 가능합니다.");
@@ -140,7 +153,17 @@ public class MemberAuthService {
     public void updatePassword(MemberPasswordPatchDto memberPasswordPatchDto, String memberEmail) throws IllegalAccessException {
         Member member = memberRepository.findByUsername(memberEmail).orElseThrow(()
                 -> new IllegalAccessException("해당 유저가 없습니다."));
-        member.setPassword(passwordEncoder.encode(memberPasswordPatchDto.getPassword()));
+        String password = memberPasswordPatchDto.getPassword();
+        if (isRegexPassword(password)) { // 비밀번호 정규식
+            String oldPassword = getMyInfo(memberEmail).getPassword();
+            if (checkSamePassword(password, oldPassword)) {
+                member.setPassword(passwordEncoder.encode(memberPasswordPatchDto.getPassword()));
+            } else {
+                throw new IllegalAccessException("이전 비밀번호와 동일합니다.");
+            }
+        } else {
+            throw new IllegalAccessException("잘못된 비밀번호 입력 형식입니다.");
+        }
         memberRepository.save(member);
     }
 
@@ -150,7 +173,7 @@ public class MemberAuthService {
         Member member = memberRepository.findByUsername(memberEmail).orElseThrow(()
                 -> new IllegalAccessException("해당 유저가 없습니다."));
         String phoneNum = memberPhonePatchDto.getPhoneNum();
-        if(isRegexPhoneNum(phoneNum)) { // 전화번호 정규식
+        if (isRegexPhoneNum(phoneNum)) { // 전화번호 정규식
             phoneNum = makePhoneNum(phoneNum); // '-' 제거
         } else {
             throw new IllegalAccessException("잘못된 전화번호 입력 형식입니다.");
@@ -168,27 +191,27 @@ public class MemberAuthService {
     }
 
     // 계좌 생성 이벤트 발생
-    public void createAccountEvent(Account account, Member member){
+    public void createAccountEvent(Account account, Member member) {
         MemberCreateEvent memberCreateEvent = new MemberCreateEvent(member.getUsername(), account.getBankName(), account.getAccountNum(), account.getBalance());
         applicationEventPublisher.publishEvent(memberCreateEvent);
     }
 
     // 멤버 계좌 잔액 차감 이벤트 발생
-    public void memberAccountDeductEvent(String email, Long balance){
+    public void memberAccountDeductEvent(String email, Long balance) {
         MemberCashChargeEvent memberCashChargeEvent = new MemberCashChargeEvent(email, balance);
         applicationEventPublisher.publishEvent(memberCashChargeEvent);
     }
 
     // 충전하려는 금액 양의 정수인지 확인
-    public boolean validateCash(long amount){
-        if(amount <= 0){
+    public boolean validateCash(long amount) {
+        if (amount <= 0) {
             throw new IllegalArgumentException("0원 이하는 충전이 불가능합니다.");
         }
         return true;
     }
 
     // 전화번호 정규식
-    public static boolean isRegexPhoneNum(String phoneNum){
+    public static boolean isRegexPhoneNum(String phoneNum) {
         String regex = "^[0-9-]*$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(phoneNum);
@@ -200,10 +223,49 @@ public class MemberAuthService {
     }
 
     // 전화번호에서 '-'을 제거해주는 역할
-    public static String makePhoneNum(String phoneNum){
-        if(phoneNum.contains("-")){
+    public static String makePhoneNum(String phoneNum) {
+        if (phoneNum.contains("-")) {
             phoneNum = phoneNum.replaceAll("-", "");
         }
         return phoneNum;
     }
+
+    //    비밀번호 정규식
+//    적어도 하나의 알파벳 (A-Za-z) 문자를 포함
+//    적어도 하나의 숫자 (0-9) 문자를 포함
+//    적어도 하나의 특수 문자 ($@$!%*#?&)를 포함
+//    오직 알파벳 문자, 숫자 문자 또는 지정된 특수 문자로만 구성
+//    길이는 8 ~ 20자 사이
+    public static boolean isRegexPassword(String password) {
+        String regex = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z[0-9]$@$!%*#?&]{8,20}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+
+        if (matcher.matches()) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isRegexEmail(String email){
+        String regex = "^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+
+        if (matcher.matches()) {
+            return true;
+        }
+        return false;
+    }
+
+
+    // 과거 비밀번호와 현재 비밀번호 같은지 판별
+    public boolean checkSamePassword(String newPassword, String oldPassword) {
+        if (passwordEncoder.matches(newPassword, oldPassword)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
