9d67c6fb330b9774af6c7bd748c2dce47bea9fed
JaeHeon Jeong <76707549+drdd1120@users.noreply.github.com>
2023-08-19 19:00:26 +0900

    Refactor: 검색 기능 이미지, 가격, 좋아요 안나오는 것 수정, controller 코드 -> service로 분리 (#68)

--- src/main/java/com/example/demo/src/search/controller/SearchController.java
+++ src/main/java/com/example/demo/src/search/controller/SearchController.java
@@ -1,6 +1,7 @@
 package com.example.demo.src.search.controller;
 
 import com.example.demo.src.product.domain.Product;
+import com.example.demo.src.product.dto.DisplayProductRes;
 import com.example.demo.src.search.dto.ResponseMultiSearch;
 import com.example.demo.src.search.mapper.SearchMapper;
 import com.example.demo.src.search.service.SearchService;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,12 +29,7 @@ public class SearchController {
                                  @RequestParam(defaultValue = "1") int page,
                                  @RequestParam(defaultValue = "100") int size){
 
-        Page<Product> pageMedias = searchService.search(q, page-1, size);
-        List<Product> medias = pageMedias.getContent();
-
-        // 총 결과 수를 가져옵니다.
-        long totalResults = pageMedias.getTotalElements();
-
-        return new ResponseEntity<>(new ResponseMultiSearch(searchMapper.displayProductRes(medias), page, pageMedias.getTotalPages(), totalResults), HttpStatus.OK);
+        ResponseMultiSearch response = searchService.search(q, page, size);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
