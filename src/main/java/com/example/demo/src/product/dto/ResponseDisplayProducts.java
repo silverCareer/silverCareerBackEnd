d29dbd69b9559d48e2d5be32706620b8a1dca8a3
kylesung0520 <kylesung0520@gmail.com>
2023-08-23 22:46:04 +0900

    Feature: 카테고리별 상품 조회 및 상품 상세 조회 시, 현재 로그인한 계정의 해당 상품에 대한 좋아요 여부를 반환

--- src/main/java/com/example/demo/src/product/dto/ResponseDisplayProducts.java
+++ src/main/java/com/example/demo/src/product/dto/ResponseDisplayProducts.java
@@ -12,16 +12,18 @@ public class ResponseDisplayProducts {
     private String productDescription;
     private String category;
     private String productImage;
+    private boolean isLiked;
     private Long productLikes;
     private Long productPrice;
 
-    public static ResponseDisplayProducts of(Product product){
+    public static ResponseDisplayProducts of(Product product, boolean isLiked){
         return ResponseDisplayProducts.builder()
                 .productIdx(product.getProductIdx())
                 .productName(product.getProductName())
                 .productDescription(product.getDescription())
                 .category(product.getCategory())
                 .productImage(product.getImage())
+                .isLiked(isLiked)
                 .productLikes(product.getLikes())
                 .productPrice(product.getPrice())
                 .build();
