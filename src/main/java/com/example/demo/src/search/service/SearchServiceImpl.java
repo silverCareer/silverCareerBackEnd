1de51ba1b79400bcda76c819d6b3cf00e65c1d31
kylesung0520 <kylesung0520@gmail.com>
2023-08-24 09:18:54 +0900

    Feature: 상품 검색 시 상품별 현재 로그인 계정의 좋아요 여부 반환

--- src/main/java/com/example/demo/src/search/service/SearchServiceImpl.java
+++ src/main/java/com/example/demo/src/search/service/SearchServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.demo.src.search.service;
 
 import com.example.demo.global.exception.dto.CommonResponse;
 import com.example.demo.global.exception.error.search.NotFoundSearchException;
+import com.example.demo.src.likes.repository.LikeRepository;
 import com.example.demo.src.product.domain.Product;
 import com.example.demo.src.product.dto.ResponseDisplayProducts;
 import com.example.demo.src.product.repository.ProductRepository;
@@ -11,6 +12,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,26 +24,34 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class SearchServiceImpl implements SearchService{
     private final ProductRepository productRepository;
+    private final LikeRepository likeRepository;
 
     @Override
     @Transactional
-    public ResponseEntity<CommonResponse> search(String query, int page, int size){
+    public ResponseEntity<CommonResponse> search(final Authentication authentication, final String query, final int page, final int size){
         Page<Product> searchProduct = productRepository.findByProductName(query, PageRequest.of(page - 1, size, Sort.by("productIdx").descending()));
         if(searchProduct.isEmpty() || searchProduct == null){
             throw new NotFoundSearchException();
         }
-        List<Product> medias = searchProduct.getContent();
-        long totalResults = searchProduct.getTotalElements();
-        List<ResponseDisplayProducts> searchListDto = mapToDisplayProductRes(medias);
+        List<ResponseDisplayProducts> searchListDto = searchProduct.getContent().stream()
+                .map(product -> {
+                    boolean isLiked = false;
+                    if (authentication != null && authentication.isAuthenticated()){
+                        String authority = authentication.getAuthorities().stream()
+                                .findFirst().map(GrantedAuthority::getAuthority).orElse("");
+                        if("ROLE_MENTEE".equals(authority)){
+                            String memberEmail = authentication.getName();
+                            isLiked = likeRepository.existsByProductIdxAndMemberEmail(product.getProductIdx(), memberEmail);
+                        }
+                    }
+                    return ResponseDisplayProducts.of(product, isLiked);
+                })
+                .collect(Collectors.toList());
+
         ResponseMultiSearch<ResponseDisplayProducts> response =
-                new ResponseMultiSearch<>(searchListDto, page, searchProduct.getTotalPages(), totalResults);
+                new ResponseMultiSearch<>(searchListDto, page, searchProduct.getTotalPages(), searchProduct.getTotalElements());
+
         return ResponseEntity.ok().body(
                 CommonResponse.builder().success(true).response(response).build());
-    }
-
-    private List<ResponseDisplayProducts> mapToDisplayProductRes(List<Product> products) {
-        return products.stream()
-                .map(ResponseDisplayProducts::of)
-                .collect(Collectors.toList());
     }
 }
