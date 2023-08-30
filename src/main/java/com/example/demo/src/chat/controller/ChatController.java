b318ace487217cc638dccdbf6dac7b6f0c47449c
sss_hun <88662101+seonghunhan@users.noreply.github.com>
2023-08-30 12:51:40 +0900

    Fix : chat

--- src/main/java/com/example/demo/src/chat/controller/ChatController.java
+++ src/main/java/com/example/demo/src/chat/controller/ChatController.java
@@ -2,15 +2,19 @@ package com.example.demo.src.chat.controller;
 
 import com.example.demo.src.chat.domain.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 public class ChatController {
 
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
     @MessageMapping("/sendMessage")
-    @SendTo("/topic/messages")
-    public Message sendMessage(Message message) {
-        return message;
+    public void sendMessage(Message message) {
+        // message.getChatId() 로 채팅방 ID를 가져올 수 있습니다.
+        simpMessagingTemplate.convertAndSend("/topic/messages/" + message.getReceiver(), message);
     }
 }
